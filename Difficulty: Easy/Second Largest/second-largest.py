a8c214289b213e0770f769ddc8bb133d57767ec5
Muhammad-Kashif-javed <155528616+Muhammad-Kashif-javed@users.noreply.github.com>
2025-04-19 14:49:41 +0500

    Update second-largest.py

--- Difficulty: Easy/Second Largest/second-largest.py
+++ Difficulty: Easy/Second Largest/second-largest.py
@@ -11,22 +11,15 @@ class Solution:
                 secondlargest = arr[i]
         return secondlargest
             
+Two separate loops over the array of size n, Each loop runs O(n) times, Total time = O(n + n) = O(n)
+Time Complexity = O(n)
+
+Space Complexity:
+we use only a constant number of variables (largest, secondlargest, n), No additional data structures are used
+Space Complexity = O(1) (constant space)
+
             
 
 
     
 
-
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        arr = list(map(int, input().split()))
-        ob = Solution()
-        ans = ob.getSecondLargest(arr)
-        print(ans)
-        print("~")
-# } Driver Code Ends
